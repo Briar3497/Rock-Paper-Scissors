6269bea1723ef07299717fa5b94cc6ac95adfd26
Briar Lilly <silvermoon3497@gmail.com>
2025-09-16 21:29:15 -0500

    creates game screen text and layout

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -12,11 +12,34 @@ running = True
 player_wins = 0
 computer_wins = 0
 
+white = 255, 255, 255
+black = 0, 0, 0
+dark_grey = 50, 50, 50
+
 font = pygame.font.SysFont('Arial', 20)
-opening_line = font.render('Let\'s play rock, paper, or scissors\n'
-                                'First to 3 wins\n'
-                                '\n', True, (255, 255, 255))
-text_rect = opening_line.get_rect(center=(screen_width // 2, 200))
+line_start_x = 180
+line_start_y = 100
+line_height = font.get_linesize()
+
+opening_line = 'Let\'s play rock, paper, or scissors\nFirst to 3 wins\n\n'
+lines = opening_line.split('\n')
+
+rect_size = (75, 50)
+rock_pos = (150, 200)
+paper_pos = (250, 200)
+scissors_pos = (350, 200)
+
+rock_rect = pygame.Rect(rock_pos, rect_size)
+rock_text_surface = font.render('Rock', True, white)
+rock_choice = rock_text_surface.get_rect(center=rock_rect.center)
+
+paper_rect = pygame.Rect(paper_pos, rect_size)
+paper_text_surface = font.render('Paper', True, white)
+paper_choice = paper_text_surface.get_rect(center=paper_rect.center)
+
+scissors_rect = pygame.Rect(scissors_pos, rect_size)
+scissors_text_surface = font.render('Scissors', True, white)
+scissors_choice = scissors_text_surface.get_rect(center=scissors_rect.center)
 
 running = True
 while running:
@@ -24,8 +47,19 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
-    screen.blit(opening_line, text_rect)
+    screen.fill(black)
+    for i, line in enumerate(lines):
+        text_surface = font.render(line, True, white)
+        screen.blit(text_surface, (line_start_x, line_start_y + i * line_height))
+
+    pygame.draw.rect(screen, dark_grey, rock_rect)
+    screen.blit(rock_text_surface, rock_choice)
+
+    pygame.draw.rect(screen, dark_grey, paper_rect)
+    screen.blit(paper_text_surface, paper_choice)
+
+    pygame.draw.rect(screen, dark_grey, scissors_rect)
+    screen.blit(scissors_text_surface, scissors_choice)
 
     pygame.display.flip()
 
@@ -45,10 +79,10 @@ while player_wins < 3 and computer_wins < 3:
   else:
     winner = "Computer"
 
-  if (winner == "Player"):
+  if winner == "Player":
     player_wins += 1
     print("You won")
-  elif (winner == "Computer"):
+  elif winner == "Computer":
     computer_wins += 1
     print("Computer won")
   else:
